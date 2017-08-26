a8e70eb7fde45cf43cc6d0c9a686e4f8ba3ec3a9
Aziz <abo3zah@gmail.com>
2017-08-26 11:06:25 +0000

    functions added from GUIZERO

--- HelloWorldGUI.py
+++ HelloWorldGUI.py
@@ -1,4 +1,30 @@
-from guizero import App, Text
-app = App(title="Hello World")
-welcome_message = Text(app, text="Welcome to my app", size=40)
+from guizero import *
+
+# confirmation message after closure
+def do_this_on_close():
+    if yesno("Close", "Do you want to quit?"):
+        app.destroy()
+
+# setting app the name
+def say_my_name():
+    if my_name.get()=="":
+        error("OH No", "Where is your name??")
+    else:
+        welcome_message.set(my_name.get())
+
+#initializing the app
+app = App(title="Hello World", layout="grid")
+
+#adding a text and setting the default value
+welcome_message = Text(app, text="Welcome to my app", color="Red", grid=[0,0])
+
+#adding a textbox with intial setting
+my_name = TextBox(app, "Type your name", width=50, grid=[1,0])
+
+#adding a pushbutton
+update_text= PushButton(app, command=say_my_name, text="Dispaly your name", grid=[2,0])
+
+# When user tries to close the app
+app.on_close(do_this_on_close)
+
 app.display()
